196b5c283eff7aee15a898050ac312e0dbe6b058
EgNik07 <egnik07@gmail.com>
2022-11-07 18:54:00 +0400

    v1.0

---
+++ express/script.js
@@ -0,0 +1,129 @@
+var grassArr = [];
+var redgrassArr = []; 
+var eatersArr = []; 
+var eaterblueArr = [];
+var eaterredArr = [];
+var eaterdarkArr = [];
+var side = 28;
+var matrix = [
+    [5, 5, 5, 6, 1, 6, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 2, 0, 1, 0, 1, 0, 0],
+    [0, 0, 2, 5, 5, 6, 1, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 1, 0, 0],
+    [0, 1, 3, 0, 1, 5, 1, 0, 3, 0, 0, 0, 4, 2, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 2, 0, 1, 1, 0, 4, 0, 0, 1, 4, 4, 1, 0, 0, 0, 0, 0, 1, 2, 0, 2, 0, 1, 0, 0],
+    [0, 2, 0, 0, 0, 0, 4, 4, 4, 2, 0, 2, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 2, 0, 1, 3, 2, 4, 4, 0, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 2, 0, 0],
+    [0, 1, 0, 0, 1, 1, 0, 4, 4, 2, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 1],
+    [0, 1, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 1, 2, 0, 1, 0, 1, 0, 0],
+    [0, 2, 0, 0, 2, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 2, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 2, 0, 1, 0, 0, 0, 1, 2, 0, 0, 2, 0, 1, 0],
+    [0, 1, 0, 0, 1, 0, 2, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 1, 0, 2, 0, 0],
+    [3, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 2, 0, 1, 0, 1, 0, 0],
+    [0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0, 0, 0, 1, 2, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 1, 0, 3, 0, 0, 4, 4, 2, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 2, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 2, 0, 2, 0, 1, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [1, 0, 2, 0, 1, 0, 2, 0, 0, 0, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 2, 0, 0],
+    [0, 1, 0, 0, 1, 1, 0, 0, 0, 2, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 1],
+    [0, 2, 1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 1, 0, 1, 0, 0, 2, 1, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 2, 0, 0, 0, 1, 0, 4, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 1, 2, 0, 1, 0, 1, 0, 0],
+    [0, 2, 0, 0, 2, 0, 1, 0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 1, 2, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 2, 0, 1, 0, 0, 0, 1, 2, 0, 0, 2, 0, 1, 0],
+    [0, 1, 0, 0, 1, 0, 2, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 1, 0, 2, 0, 0]
+]
+
+function setup() {
+    noStroke();
+    frameRate(1);
+    createCanvas(matrix[0].length * side, matrix.length * side); 
+    background('#acacac');
+
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[y].length; x++) {
+
+            if (matrix[y][x] == 1) {
+                var grass = new Grass(x, y);
+                grassArr.push(grass);
+            }
+
+            else if (matrix[y][x] == 2) {
+                var eater = new GrassEater(x, y);
+                eatersArr.push(eater);
+            }
+            else if (matrix[y][x] == 3) {
+                var eaterbluE = new Eaterblue(x, y);
+                eaterblueArr.push(eaterbluE);
+
+            }
+            else if (matrix[y][x] == 4) {
+                var eaterred = new Eaterred(x, y);
+                eaterredArr.push(eaterred);
+            }
+            else if (matrix[y][x] == 5) {
+                var eaterdark = new Eaterdark(x, y);
+                eaterdarkArr.push(eaterdark);
+            }
+            else if (matrix[y][x] == 6) {
+                var redgrass = new RedGrass(x, y);
+                redgrassArr.push(redgrass);
+            }
+
+        }
+    }
+}
+
+function draw() {
+    background('#acacac');
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] == 1) {
+                fill("green");
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 3) {
+                fill("blue");
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 2) {
+                fill("yellow");
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 0) {
+                fill('#acacac');
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 4) {
+                fill('red');
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 5) {
+                fill('613a3a')
+                rect(j * side, i * side, side, side);
+            }
+            else if (matrix[i][j] == 6) {
+                fill('pink')
+                rect(j * side, i * side, side, side);
+            }
+        }
+    }
+    for (var i in grassArr) {
+        grassArr[i].mul();
+    }
+    for (var i in redgrassArr) {
+        redgrassArr[i].mul();
+    }
+
+    for (var i in eatersArr) {
+        eatersArr[i].eat();
+    }
+    for (var i in eaterblueArr) {
+        eaterblueArr[i].eat();
+    }
+    for (var i in eaterredArr) {
+        eaterredArr[i].eat();
+    }
+    for (var i in eaterdarkArr) {
+        eaterdarkArr[i].eat();
+    }
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var path = require('path');
+
+var app = express();
+
+
+app.get("/", function(req, res){
+
+    res.sendFile(path.join(__dirname, '/index.html'));
+   
+
+});
+
+
+app.use(express.static('express'));
+
+app.listen(3000, function(){
+
+console.log("Example is running on port 3000");
+
+});
