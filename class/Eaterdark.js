9254a318b9a7fd1d2af877f123638132f6309e20
EgNik07 <egnik07@gmail.com>
2022-11-08 14:01:37 +0400

    v1.1

---
+++ class/Eaterdark.js
@@ -0,0 +1,142 @@
+class Eaterdark {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+        this.energy = 5;
+        this.directions = [];
+    }
+
+    //թարմացնել շրջապատի կոորդինատները
+    updateCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    //հետազոտում է շրջապատը, որոնում է հետաքրքրող կերպարներին
+    //կերպարը որոշվում է character արգումենտով
+    chooseCell(character) {
+        this.updateCoordinates();
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+
+
+    //move() շարժվել
+    move() {
+        //որոնում է դատարկ տարածքներ
+        var emptyCells = this.chooseCell(0);
+        var cօord = random(emptyCells); // 4,3
+
+        if (cօord) {
+            var x = cօord[0];
+            var y = cօord[1];
+
+
+            //շարժվում է
+            matrix[y][x] = 5;
+            matrix[this.y][this.x] = 3;
+
+            //նոր կորդինատներ է ստանում
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+
+    //eat()-ուտել
+    eat() {
+        //հետազոտում է շրջակայքը, որոնում է սնունդ
+        var grassCells = this.chooseCell(1);
+        var coord = random(grassCells);
+
+        //եթե կա հարմար սնունդ
+        if (coord) {
+            var x = coord[0];
+            var y = coord[1];
+
+            //հիմնական մատրիցայում տեղափոխվում է կերած սննդի տեղը
+            //իր հին տեղը դնում է դատարկ վանդակ
+            matrix[y][x] = 5;
+            matrix[this.y][this.x] = 0;
+
+            //փոխում է սեփական կորդինատները օբյեկտի մեջ
+            this.x = x;
+            this.y = y;
+
+            //բազմացման գործակիցը մեծացնում է
+            this.multiply++;
+
+            //մեծացնում է էներգիան
+            this.energy++;
+
+            // սննդի զանգվածից ջնջում է կերված սնունդը
+            for (var i in eaterblueArr) {
+                if (x == eaterblueArr[i].x && y == eaterblueArr[i].y) {
+                    eaterblueArr.splice(i, 1);
+                }
+            }
+
+            //եթե պատրաստ է բազմացմանը, բազմանում է 
+            if (this.multiply == 4) {
+                this.mul()
+                this.multiply = 0;
+            }
+
+
+        } else {
+            //եթե չկա հարմար սնունդ 
+            this.move();
+            this.energy--;
+            if (this.energy <= 0) { //մահանում է, եթե էներգիան 0֊ից ցածր է
+                this.energy++
+            }
+        }
+    }
+
+    //mul() բազմանալ
+    mul() {
+        //փնտրում է դատարկ տարածք
+        var emptyCells = this.chooseCell(0);
+        var coord = random(emptyCells);
+
+        //եթե կա բազմանում է
+        if (coord) {
+            var x = coord[0];
+            var y = coord[1];
+            // this.multiply++;
+            //ստեղծում է նոր օբյեկտ (այստեղ խոտակեր) 
+            //և տեղադրում է այն խոտակերների զանգվածի մեջ
+            var newEater = new Eaterred(x, y);
+            eaterredArr.push(newEater);
+
+            //հիմնական matrix-ում կատարում է գրառում նոր խոտի մասին
+            matrix[y][x] = 2;
+        }
+        else {
+            this.move()
+        }
+    }
+
+
+
+}
---
+++ class/Grass.js
@@ -0,0 +1,53 @@
+class Grass {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+    chooseCell(character) {
+        var found = [];
+        for (var i in this.directions) {
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+        }
+        return found;
+    }
+
+
+    
+    mul() {
+        this.multiply++;
+        if (this.multiply >= 0) {
+            var emptyCells = this.chooseCell(0);
+            var coord = random(emptyCells);
+            if (coord) {
+                var x = coord[0];
+                var y = coord[1];
+
+                //ավելացնում է նոր խոտ խոտերի զանգվածում
+                var newGrass = new Grass(x, y);
+                grassArr.push(newGrass);
+
+                //ավելացնում է նոր խոտ մատրիցում
+                matrix[y][x] = 1;
+                this.multiply = 0;
+            }
+        }
+    }
+}
